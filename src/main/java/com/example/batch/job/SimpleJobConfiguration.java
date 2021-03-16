98a59c451e41ac508b497ffb793dc472da7e61ac
hongyeongjune <hongyeongjune@naver.com>
2021-03-16 15:22:46 +0900

    메타 테이블 엿보기

--- src/main/java/com/example/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/batch/job/SimpleJobConfiguration.java
@@ -24,6 +24,7 @@ public class SimpleJobConfiguration {
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
@@ -38,4 +39,17 @@ public class SimpleJobConfiguration {
                 })
                 .build();
     }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
 }
