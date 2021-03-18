adbf2572f574aabc3335c1dce181a3916bac160d
hongyeongjune <hongyeongjune@naver.com>
2021-03-18 17:40:31 +0900

    Item Reader, Writer -> JdbcCursor , JdbcPaging , JpaPaging

---
+++ src/main/java/com/example/batch/practice/infrastructure/persistence/entity/Pay3.java
@@ -0,0 +1,33 @@
+package com.example.batch.practice.infrastructure.persistence.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Pay3 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long amount;
+    private String txName;
+    private LocalDateTime txDateTime;
+
+    public Pay3(Long amount, String txName, LocalDateTime txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+}
